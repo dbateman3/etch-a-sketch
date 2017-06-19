7b29a20b282f0182220398094e93412a51ce67bb
Dan <dbateman0319@starkstate.net>
2017-06-18 22:18:59 -0500

    working on hover effect

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	var $gridLimit = 256;
 
 	for(i = 1; i <= $gridLimit; i++) {
-		$('#container').append('<div class = "grid"></div>');
+		$('#container').append('<div id = "grid"></div>');
 	}
+
+	$('#grid').mouseenter(function() {
+		$('#grid').addClass("hover");
+	});
 });
